2a9663471f4143eec29ca2fd98fa0ac2718aebaf
AbuzarFX <70717700+AbuzarFX@users.noreply.github.com>
2021-02-04 12:37:05 +0530

    Update prefix.js

--- commands/General/prefix.js
+++ commands/General/prefix.js
@@ -1,40 +1,57 @@
-const db = require('quick.db');
+const db = require("quick.db")
+const { MessageEmbed } = require("discord.js")
+const { PREFIX } = require("../../config")
 
 module.exports = {
     config: {
-        name: "setprefix",
-        aliases: ['sp', 'prefix'],
-        category: "moderation",
-        description: "Sets Custom Prefix",
-        usage: "[prefix]",
-        accessableby: 'Administrators'
+        name: "prefix",
+        description: "Chnage/Know The server's Prefix or the Global Prefix",
+        usage: "m/prefix <new prefix/reset>",
+        example: "1) m/prefix = \n2) m/prefix reset",
+        aliases: ["prefix"]
     },
+
     run: async (bot, message, args) => {
-        if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("**You Do Not Have Sufficient Permissions To Use This Command!**")
+        let option = args[0];
 
-        if (!args[0]) {
-          let b = await db.fetch(`prefix_${message.guild.id}`);
-          if (b) {
-        return message.channel.send(
-          `**Prefix Of This Server is \`${b}\`**`
-        );
-      } else return message.channel.send("**Please Enter A Prefix To Set!**");
-    } 
-      
-        try {
-
-            let a = args.join(' ');
-            let b = await db.fetch(`prefix_${message.guild.id}`)
-
-            if (a === b) {
-                return message.channel.send('**This is Already The Server Prefix!**')
-            } else {
-                db.set(`prefix_${message.guild.id}`, a)
-
-                return message.channel.send(`**Successfuly Set Server Prefix To \`${a}\`**`)
+            //PERMISSION
+     if(!message.member.hasPermission("MANAGE_GUILD")) {
+                return message.channel.send("You are not allowed or do not have permission to change prefix")
+              }
+            
+            if(!option) {
+                prefix = db.fetch(`prefix_${message.guild.id}`)
+                if (!prefix) prefix = PREFIX;
+                let prefEmbed = new MessageEmbed()
+                .setColor('YELLOW')
+                .setThumbnail(message.guild.iconURL())
+                .setDescription(`\nMy prefix for \`${message.guild.name}\`  is  **` + `  \`${prefix}\` \n**Type \`${prefix}help\` for help.`)
+              
+              message.channel.send(prefEmbed);
             }
-        } catch (e) {
-            console.log(e)
+
+            if(option.toLowerCase() === "reset") {
+                db.delete(`prefix_${message.guild.id}`)
+                return await message.channel.send("Reset prefix <:tick:788102360936218685>")
+            }
+            
+            if(args[1]) {
+              return message.channel.send("You cannot set prefix a double argument.")
+            }
+            
+            if(args[0].length > 4) {
+              return message.channel.send("You cannot have a prefix more than 4 characters")
+            }
+            
+            if(args.join("") === PREFIX) {
+              db.delete(`prefix_${message.guild.id}`)
+             return await message.channel.send("Reset prefix <:tick:788102360936218685>")
+            }
+            
+            db.set(`prefix_${message.guild.id}`, args[0])
+          await message.channel.send(`Done <:tick:788102360936218685>. Bot prefix set to \`${args[0]}\``)
+            
+
         }
+        
     }
-}
