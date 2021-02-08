e0ad8af683803cfb2f295ae1327a08305b3fd061
AbuzarFX <70717700+AbuzarFX@users.noreply.github.com>
2021-02-08 21:20:05 +0530

    Update help.js

--- commands/General/help.js
+++ commands/General/help.js
@@ -22,6 +22,7 @@ module.exports = {
         .setDescription(`Akinator is a computer game and mobile app by French company Elokence. During gameplay, it attempts to determine what fictional or real-life character, object, film, television show, or animal the player is thinking of by asking a series of questions (like the game Twenty Questions).\n\n\n<:defi1:804797586438357003> \`aki ping\` - Returns the ping of the bot.\n\n<:defi1:804797586438357003> \`aki info\` - Returns the information of the bot.\n\n<:defi1:804797586438357003> \`aki about\` - Returns the information of the Developer.\n\n<:defi1:804797586438357003> \`aki akinator [person, object, animal]\` - Starts the Akinator Game!\n\n<:defi1:804797586438357003> \`end\` - Ends the current akinator game.\n\n<:defi1:804797586438357003> \`aki invite [inv]\` - Returns the invite link of Akinator.\n\nYou'll understand the rest of the game as you go with it. Have fun! <:defi1:804797586438357003> `)
         .setFooter("Made with ♥ by Abuser#2824")
         .setThumbnail("https://i.pinimg.com/originals/fe/e0/24/fee0246d3c4bddd06e95b41afbf13024.png")
+        .setImage(`https://i.imgur.com/WsNXfAs.jpg`)
         .setTimestamp()
 
         message.channel.send(embed)
