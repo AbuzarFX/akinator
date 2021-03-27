4e897e049585d83005872b9814ac0397d133bfa7
Syed Abuzar <70717700+AbuzarFX@users.noreply.github.com>
2021-03-27 12:50:49 +0530

    Update stats.js

--- commands/General/stats.js
+++ commands/General/stats.js
@@ -6,11 +6,21 @@ module.exports = {
     },
     run: async (bot, message, args) => {
 
+        const promisedd = [
+            bot.shard.fetchClientValues('guilds.cache.size'),
+            bot.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)'),
+        ];
+        
+        Promise.all(promisedd)
+            .then(results => {
+                const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+                const totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+                return message.channel.send(`> Server count: ${totalGuilds} servers\n> Member count: ${totalMembers} members`);
+            })
+            .catch(console.error);
 
-        const embed = new MessageEmbed()
-        .setDescription(`Serving ${bot.users.cache.size} users on ${bot.guilds.cache.size} servers.`)
-        .setColor('GOLD')
 
-        message.reply(embed)
+
+
     }
 }
