fdd72de983795df816b90fa8ce35a563f8772764
Syed Abuzar <70717700+AbuzarFX@users.noreply.github.com>
2021-03-27 14:01:39 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -17,9 +17,8 @@ const { PREFIX } = require('../../config');
 module.exports = async bot => {
     console.log(`${bot.user.username} is available now!`)
 
-    var activities = [ `${bot.guilds.cache.size} servers`, `${bot.users.cache.size} users!` ], i = 0;
 
-    setInterval(() => bot.user.setActivity(`${PREFIX}help | ${activities[i++ % activities.length]}`, { type: "WATCHING" }),5000)
+    setInterval(() => bot.user.setActivity(`${PREFIX}help`, { type: "WATCHING" }))
 
   
   
