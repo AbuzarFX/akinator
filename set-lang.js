3fff61009e7f2bae3c41afb94df6d567e7fb98f3
Syed Abuzar <70717700+AbuzarFX@users.noreply.github.com>
2021-03-29 21:47:49 +0530

    Update set-lang.js

--- set-lang.js
+++ set-lang.js
@@ -1,28 +1,24 @@
-const db = require("quick.db")
 
+const { MessageEmbed } = require("discord.js")
 module.exports = {
     config: {
         name: "lang",
-        aliases: ["set-lang", "language"],
+        aliases: ["language"],
     },
     run: async (bot, message, args) => {
-        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send({ embed: {
-            color: "#ff0000",
-            description: "<:9636_Cross:796376380560113675> You don't have the required permissions to use this command!"
-        }})
-        
-        const lang = args[0];
-        if(!lang) return message.channel.send({ embed: {
-            
-            color: "#ff0000",
-            description: `<:9636_Cross:796376380560113675> ${await bot.translate(`Please specify a language to set!`, message)}`
-        }})
-        await db.set(`lang-${message.guild.id}`, lang);
 
-        message.channel.send({ embed: {
-            color: "GOLD",
-            description: `<:tick:788102360936218685> ${await bot.translate(`Successfully set the language to`, message)}: \`${lang}\`.`
-        }})
+        const embed = new MessageEmbed()
+        .setColor("GOLD")
+        .setAuthor(message.author.username, `https://play-lh.googleusercontent.com/rjX8LZCV-MaY3o927R59GkEwDOIRLGCXFphaOTeFFzNiYY6SQ4a-B_5t7eUPlGANrcw`)
+        .setTitle(`How to set languages? (Beta)`)
+        .setDescription(`Akinator currently supports 16 languages. It is \`English\` by default. __**To use some other language, specify the language code in the end of the command.**__\n
+        Example: \`aki aki person\` - For the default language- English.\n
+        Example: \`aki aki person fr\` - For French.`)
+        .addField(`• Available Languages`, `\`\`\`English • en\nArabic  • ar\nFrench  • fr\nChinese • cn\nGerman  • de\nSpanish • es\nFrench  • fr\nItalian • it\nJapanese • jp\nUkranian • kr\nDutch • nl\nPolish • pl\nPortuguese • pt\nRussian • ru\nTurkish • tr\nIndonesian • id
+        \`\`\``)
+        .setTimestamp()
+        .setFooter(`Akinator`)
+        message.channel.send(embed)
 
 
 
