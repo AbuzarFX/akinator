0e013897622c0b6881b0e090c795e621035d2c09
Syed Abuzar <70717700+AbuzarFX@users.noreply.github.com>
2021-04-02 23:06:40 +0530

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -4,14 +4,27 @@ const { PREFIX } = require('../../config');
 module.exports = async (bot, message) => {
 
 
-    
-    console.log(`${bot.user.username} is available now!`)
+ 
+ console.log(`${bot.user.username} is available now!`)
 
 
-    bot.user.setActivity(`${PREFIX}help`, { type: "WATCHING" })
+ bot.user.setActivity(`${PREFIX}help`, { type: "WATCHING" })
 
-    
-    
-  
-    
+ bot.guilds.cache.forEach((guild) => { //for each guild the bot is in
+ let defaultChannel = "";
+ guild.channels.cache.forEach((channel) => {
+ if(channel.type == "text" && defaultChannel == "") {
+ if(channel.permissionsFor(guild.me).has('SEND_MESSAGES', 'EMBED_LINKS')) {
+ defaultChannel = channel.id
+ }
+ }
+ })
+ setInterval (function () {
+ defaultChannel.send("** •<:defi1:804797586438357003>Like using Akinator? Vote for it on Top.gg!**\n\nhttps://top.gg/bot/804789290139385887/vote") //send it to whatever channel the bot has permissions to send on
+ }, 10000);
+ 
+})
+ 
+ 
+ 
 };
