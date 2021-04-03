1d741cc1fd8fd4a58cc0fcc743ec5a8851432310
Syed Abuzar <70717700+AbuzarFX@users.noreply.github.com>
2021-04-04 00:29:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.TOKEN = "ODA0Nzg5MjkwMTM5Mzg1ODg3.YBRcbw.GOdCNdZ3szHutVHyr_wyfzpLaj4"
+exports.TOKEN = "ODA0Nzg5MjkwMTM5Mzg1ODg3.YBRcbw.RyUtqLBJ4H-umU8BggV3pC2Vf8A"
 
 exports.PREFIX = "aki "
