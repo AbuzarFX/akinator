d34ed246c4926ecca9c4e5565014f7977b572030
Syed Abuzar <70717700+AbuzarFX@users.noreply.github.com>
2021-04-13 16:13:32 +0530

    Update lang.js

--- commands/General/lang.js
+++ commands/General/lang.js
@@ -11,6 +11,11 @@ module.exports = {
         .setColor("GOLD")
         .setThumbnail(`https://i.pinimg.com/originals/fe/e0/24/fee0246d3c4bddd06e95b41afbf13024.png`)
         .setDescription(`**Available Languages** :speech_balloon:\n\n
+Akinator currently supports 15 languages. It is \`English\` by default. __**To use some other language, specify the language code in the end of the command**__.\n
+
+        Example: \`aki aki person\` - For the default language- English.\n
+
+        Example: \`aki aki person fr\` - For French.
         :flag_sa: \`ar\` Arabic - العربية\n
         :flag_cn: \`cn\` Chinese - 中文\n
         :flag_nl: \`nl\` Dutch - Nederlands\n
@@ -25,12 +30,7 @@ module.exports = {
         :flag_ru: \`ru\` Russian - русский\n
         :flag_es: \`es\` Spanish - Español\n
         :flag_tr: \`tr\` Turkish - Türk\n
-        :flag_id: \`id\` Indonesian - Indonesian\n\n\n
-        Akinator currently supports 15 languages. It is \`English\` by default. __**To use some other language, specify the language code in the end of the command**__.\n
-
-        Example: \`aki aki person\` - For the default language- English.\n
-
-        Example: \`aki aki person fr\` - For French.`)
+        :flag_id: \`id\` Indonesian - Indonesian`)
         
 
         message.channel.send(embed)
